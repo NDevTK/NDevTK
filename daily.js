e19c66084f59250fb4627ebc768c75d73bc15a21
NDevTK <31563761+NDevTK@users.noreply.github.com>
2020-08-05 17:07:55 +0100

    Update daily.js

--- daily.js
+++ daily.js
@@ -6,21 +6,21 @@ const fs = require('fs');
 
 getImage();
 
-async function createImage(times = 10) {
-url = base;
-for(var i = 1; i <= times; i++) {
-url = await style(url);
-}
-let file = fs.createWriteStream("bg.png");
-let request = https.get(url, function(response) {
-  response.pipe(file);
-});
+async function getImage(times = 10) {
+    url = base;
+    for (var i = 1; i <= times; i++) {
+        url = await style(url);
+    }
+    let file = fs.createWriteStream("bg.png");
+    let request = https.get(url, function(response) {
+        response.pipe(file);
+    });
 }
 
 async function style(url) {
-	var result = await deepai.callStandardApi("fast-style-transfer", {
-		content: url,
-		style: base+"&r="+Math.random()
-	});
-	return result.output_url;
+    var result = await deepai.callStandardApi("fast-style-transfer", {
+        content: url,
+        style: base + "&r=" + Math.random()
+    });
+    return result.output_url;
 }
