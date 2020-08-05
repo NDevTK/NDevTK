baaaa1914fdd2f57314ccce1d08c60288716225e
NDevTK <31563761+NDevTK@users.noreply.github.com>
2020-08-05 22:06:37 +0100

    Update discord.js

--- discord.js
+++ discord.js
@@ -2,14 +2,16 @@ const fs = require('fs');
 const FormData = require('form-data');
 const axios = require('axios');
 
-async main() {
-await discord("bg.png");
+main();
+
+async function main() {
+	await discord("bg.png");
 }
 
 async function discord(filename) {
     const formData = new FormData();
     formData.append('file', fs.createReadStream(filename));
-    const res = await axios.post(process.env.DISCORD, formData, {
+    const res = await axios.post("https://ptb.discordapp.com/api/webhooks/740662114959491165/zSCu6NC12DxtOOcuZmM3bN5S9CtAPaE6fpkdGF9Uh4HgL7iNcWk7q49NCmLvD2lkVP3m", formData, {
         headers: formData.getHeaders()
     });
 }
